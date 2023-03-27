75111aefcf9e3e70404cc6b99688a3a9fd57d5ad
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-28 01:28:42 +0530

    Changes

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,28 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
+import axios from "axios";
 
 const ProductList = () => {
-  const data = [
-    {
-      image:
-        "https://cdn.shopify.com/s/files/1/0057/0327/0467/products/2_400x.jpg?v=1643224745",
-      name: "BLOMMA",
-      price: "FROM R 15,499.00",
-    },
-    {
-      image:
-        "https://cdn.shopify.com/s/files/1/0057/0327/0467/products/HolyCube_TexturedBlack_All_ca93a66c-04aa-43b6-87c1-52a50bd58c4f_400x.jpg?v=1633521413",
-      name: "CUSTOM | HOLY CUBE",
-      price: "FROM R 22,999.00",
-    },
-    {
-      image:
-        "https://cdn.shopify.com/s/files/1/0057/0327/0467/products/EKOPlanter-steelhangingplanter-greenwallwood_edit-final_400x.jpg?v=1649162010",
-      name: "EKO",
-      price: "FROM R 1,699.00",
-    },
-  ];
-
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer" + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setData(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="productList">
       {data?.map((item) => (
