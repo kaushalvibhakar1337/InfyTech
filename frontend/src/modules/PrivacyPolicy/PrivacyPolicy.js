e28a2bf00bf5fcc28bf85cbf6464ca68c37cfd3d
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-28 15:16:26 +0530

    Changes

---
+++ frontend/src/modules/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <Header title="CONTACT US" />
+    </>
+  );
+};
+
+export default Contact;
