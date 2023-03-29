0d401e40384af6ab1f8139d5036ef93bf980ac89
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-29 16:22:01 +0530

    Changes

--- frontend/src/components/MainSlider/MainSlider.js
+++ frontend/src/components/MainSlider/MainSlider.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./MainSlider.scss";
 
 const MainSlider = () => {
-  return <div className="mainSlider"></div>;
+  return (
+    <div className="mainSlider">
+      <div className="container">
+        <img src={require("../../assets/mainSliderImg1.jpg")} alt="" />
+        <img src={require("../../assets/mainSliderImg2.jpg")} alt="" />
+        <img src={require("../../assets/mainSliderImg3.jpg")} alt="" />
+        <img src={require("../../assets/mainSliderImg4.jpg")} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon"></div>
+        <div className="icon"></div>
+      </div>
+    </div>
+  );
 };
 
 export default MainSlider;
--- frontend/src/modules/Home/Home.js
+++ frontend/src/modules/Home/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import "./Home.scss";
+import MainSlider from "../../components/MainSlider/MainSlider";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="home"></div>
+      <div className="home">
+        <MainSlider />
+      </div>
       <Footer />
     </>
   );
