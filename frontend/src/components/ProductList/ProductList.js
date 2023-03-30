3c93f049b5edfb4d309a93c43537784ab7c309b0
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-30 13:17:12 +0530

    Changes

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,31 +1,71 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
-import axios from "axios";
+import "./ProductList.scss";
 
 const ProductList = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          process.env.REACT_APP_API_URL + "/products?populate=*",
-          {
-            headers: {
-              Authorization: "bearer" + process.env.REACT_APP_API_TOKEN,
-            },
-          }
-        );
-        setData(res.data.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
+  const data = [
+    {
+      id: 1,
+      img1: "mainSliderImg1.jpg",
+      img2: "mainSliderImg4.png",
+      name: "KEYBOARD",
+      price: "R 199.99",
+    },
+    {
+      id: 2,
+      img1: "mainSliderImg2.jpg",
+      img2: "mainSliderImg3.jpg",
+      name: "MOUSE",
+      price: "FROM R 399.00",
+    },
+    {
+      id: 3,
+      img1: "mainSliderImg3.jpg",
+      img2: "mainSliderImg1.jpg",
+      name: "CPU",
+      price: "FROM R 799.99",
+    },
+    {
+      id: 4,
+      img1: "mainSliderImg4.png",
+      img2: "mainSliderImg2.jpg",
+      name: "HEADPHONES",
+      price: "R 99.99",
+    },
+    {
+      id: 5,
+      img1: "mainSliderImg1.jpg",
+      img2: "mainSliderImg4.png",
+      name: "KEYBOARD",
+      price: "R 199.99",
+    },
+    {
+      id: 6,
+      img1: "mainSliderImg2.jpg",
+      img2: "mainSliderImg3.jpg",
+      name: "MOUSE",
+      price: "FROM R 399.00",
+    },
+    {
+      id: 7,
+      img1: "mainSliderImg3.jpg",
+      img2: "mainSliderImg1.jpg",
+      name: "CPU",
+      price: "FROM R 799.99",
+    },
+    {
+      id: 8,
+      img1: "mainSliderImg4.png",
+      img2: "mainSliderImg2.jpg",
+      name: "HEADPHONES",
+      price: "R 99.99",
+    },
+  ];
+
   return (
     <div className="productList">
-      {data?.map((item) => (
-        <ProductCard item={item} key={item.name} />
+      {data.map((item) => (
+        <ProductCard item={item} key={data.id} />
       ))}
     </div>
   );
--- frontend/src/components/ProductSlider/ProductSlider.js
+++ frontend/src/components/ProductSlider/ProductSlider.js
@@ -1,74 +1,7 @@
 import React from "react";
-import ProductCard from "../ProductCard/ProductCard";
-import "./ProductSlider.scss";
 
 const ProductSlider = () => {
-  const data = [
-    {
-      id: 1,
-      img1: "mainSliderImg1.jpg",
-      img2: "mainSliderImg4.png",
-      name: "KEYBOARD",
-      price: "R 199.99",
-    },
-    {
-      id: 2,
-      img1: "mainSliderImg2.jpg",
-      img2: "mainSliderImg3.jpg",
-      name: "MOUSE",
-      price: "FROM R 399.00",
-    },
-    {
-      id: 3,
-      img1: "mainSliderImg3.jpg",
-      img2: "mainSliderImg1.jpg",
-      name: "CPU",
-      price: "FROM R 799.99",
-    },
-    {
-      id: 4,
-      img1: "mainSliderImg4.png",
-      img2: "mainSliderImg2.jpg",
-      name: "HEADPHONES",
-      price: "R 99.99",
-    },
-    {
-      id: 5,
-      img1: "mainSliderImg1.jpg",
-      img2: "mainSliderImg4.png",
-      name: "KEYBOARD",
-      price: "R 199.99",
-    },
-    {
-      id: 6,
-      img1: "mainSliderImg2.jpg",
-      img2: "mainSliderImg3.jpg",
-      name: "MOUSE",
-      price: "FROM R 399.00",
-    },
-    {
-      id: 7,
-      img1: "mainSliderImg3.jpg",
-      img2: "mainSliderImg1.jpg",
-      name: "CPU",
-      price: "FROM R 799.99",
-    },
-    {
-      id: 8,
-      img1: "mainSliderImg4.png",
-      img2: "mainSliderImg2.jpg",
-      name: "HEADPHONES",
-      price: "R 99.99",
-    },
-  ];
-
-  return (
-    <div className="productSlider">
-      {data.map((item) => (
-        <ProductCard item={item} key={data.id} />
-      ))}
-    </div>
-  );
+  return <div className="productSlider"></div>;
 };
 
 export default ProductSlider;
