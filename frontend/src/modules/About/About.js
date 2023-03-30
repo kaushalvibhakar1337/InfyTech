47720b8252a2c5bd04012c28b4e61cc09054ecdb
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-30 14:15:10 +0530

    Changes

--- frontend/src/modules/About/About.js
+++ frontend/src/modules/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import "./About.scss";
 
 const About = () => {
   return (
--- frontend/src/modules/Contact/Contact.js
+++ frontend/src/modules/Contact/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
