4e6124faf69597b4782209a722e2d3512f35104a
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-30 14:43:11 +0530

    Changes

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./ProductCard.scss";
 
 const ProductCard = ({ item }) => {
   return (
     <div className="productCard">
-      <img
-        src={require(`../../assets/home/${item.img1}`)}
-        alt={item.id}
-        className="img"
-      />
-      <span className="name">{item.name}</span>
+      <NavLink to="/products/1">
+        <div className="image">
+          <img
+            src={require(`../../assets/home/${item.img1}`)}
+            alt={item.id}
+            className="img1"
+          />
+          <img
+            src={require(`../../assets/home/${item.img2}`)}
+            alt={item.id}
+            className="img2"
+          />
+        </div>
+      </NavLink>
+      <NavLink to="/products/1">
+        <span className="name">{item.name}</span>
+      </NavLink>
       <span className="price">{item.price}</span>
     </div>
   );
