abcfcbc0b1e709c987d5c3534b5fb73c89a9401e
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-03-31 14:18:28 +0530

    Changes

--- frontend/src/modules/ProductDetails/ProductDetails.js
+++ frontend/src/modules/ProductDetails/ProductDetails.js
@@ -4,10 +4,22 @@ import Navbar from "../../components/Navbar/Navbar";
 import "./ProductDetails.scss";
 
 const ProductDetails = () => {
+  const images = ["DemoImg1.jpg", "DemoImg2.jpg", "DemoImg3.jpg"];
   return (
     <>
       <Navbar />
-      <div className="productDetails">Product Details Page</div>
+      <div className="productDetails">
+        <div className="left">
+          <div className="images">
+            <img src={require(`../../assets/${images[1]}`)} alt="" />
+            <img src={require(`../../assets/${images[2]}`)} alt="" />
+          </div>
+          <div className="mainImg">
+            <img src={require(`../../assets/${images[0]}`)} alt="" />
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
       <Footer />
     </>
   );
