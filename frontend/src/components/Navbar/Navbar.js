98d2f48c0652ea6181dd329071566bad974ae6b0
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-02 15:56:43 +0530

    Changes

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -20,16 +20,16 @@ function Navbar() {
             </NavLink>
             <div className="dropdown_content">
               <span>PC Components</span>
-              <NavLink to="/products/8">Processor</NavLink>
-              <NavLink to="/products/9">Motherboard</NavLink>
+              <NavLink to="/products/1">Processor</NavLink>
+              <NavLink to="/products/2">Motherboard</NavLink>
               <NavLink to="/products/3">Graphics Card</NavLink>
               <NavLink to="/products/4">Power Supply Units</NavLink>
               <NavLink to="/products/5">PC Cabinets</NavLink>
               <NavLink to="/products/6">RAM</NavLink>
               <NavLink to="/products/7">SSD</NavLink>
               <span>Peripherals</span>
-              <NavLink to="/products/1">Mouse</NavLink>
-              <NavLink to="/products/2">Keyboard</NavLink>
+              <NavLink to="/products/8">Mouse</NavLink>
+              <NavLink to="/products/9">Keyboard</NavLink>
               <NavLink to="/products/10">Headphones</NavLink>
               <NavLink to="/products/11">Mousepad</NavLink>
               <NavLink to="/products/12">Monitors</NavLink>
