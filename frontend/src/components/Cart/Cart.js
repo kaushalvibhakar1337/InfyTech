e9be7b4878ac3a2cee382f3c15ffb6ed7e5a2714
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-03 17:35:52 +0530

    Changes

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart } from "../../redux/cartReducer";
 import { loadStripe } from "@stripe/stripe-js";
+import { makeRequest } from "../../makeRequest";
 import "./Cart.scss";
 
 const Cart = ({ closeCart }) => {
