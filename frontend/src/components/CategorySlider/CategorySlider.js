19d69503ba480871d13810538e10980fbaf89e02
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-05 13:34:03 +0530

    Changes

--- frontend/src/components/CategorySlider/CategorySlider.js
+++ frontend/src/components/CategorySlider/CategorySlider.js
@@ -6,6 +6,7 @@ const CategorySlider = () => {
     <div className="categorySlider">
       <fieldset>
         <legend>SHOP NOW</legend>
+        <div className="slider">Hello World!</div>
       </fieldset>
     </div>
   );
