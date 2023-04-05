16c70ed89a849de5674b03929a04ce9b7975cbc4
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-05 16:04:06 +0530

    Changes

---
+++ frontend/src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6FGJDbLzwPGPQU390RPk6mwhPi3Euedg",
+  authDomain: "infytech-b1831.firebaseapp.com",
+  projectId: "infytech-b1831",
+  storageBucket: "infytech-b1831.appspot.com",
+  messagingSenderId: "564496771333",
+  appId: "1:564496771333:web:e050a3d598d4ccf1b0ef6c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
