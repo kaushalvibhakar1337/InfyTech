b272de7a1ba71ceb8e9c820359ca1740d091309d
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-05 11:51:26 +0530

    Changes

--- frontend/src/components/CategorySlider/CategorySlider.js
+++ frontend/src/components/CategorySlider/CategorySlider.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ProductCard from "../ProductCard/ProductCard";
 import "./CategorySlider.scss";
 
 const CategorySlider = () => {
   return (
     <div className="categorySlider">
-      <ProductCard />
+      <fieldset>
+        <legend>SHOP NOW</legend>
+      </fieldset>
     </div>
   );
 };
