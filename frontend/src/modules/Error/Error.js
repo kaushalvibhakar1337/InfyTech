43d54af24f9fac3c69584fe846ce1cc717a30f80
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-06 12:19:46 +0530

    Changes

--- frontend/src/modules/Error/Error.js
+++ frontend/src/modules/Error/Error.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 import "./Error.scss";
 
 const Error = () => {
   return (
-    <>
+    <div className="error">
       <Navbar />
-      <div className="error">
+      <div className="waypoint">
         <p className="top">404</p>
         <p className="middle">The page you are looking for cannot be found.</p>
         <NavLink to="/">
@@ -16,7 +17,8 @@ const Error = () => {
           </button>
         </NavLink>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
