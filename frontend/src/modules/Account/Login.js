e4ae8c3edc1686bfa9ccf8abdc38e64b46d3fdfa
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-06 15:43:52 +0530

    Changes

--- frontend/src/modules/Account/Login.js
+++ frontend/src/modules/Account/Login.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import "./Account.scss";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    console.log("Your Email is : " + email);
+    console.log("Your Password is : " + password);
+  };
+
   return (
     <div className="login">
       <Navbar />
       <div className="form" style={{ marginTop: "15px" }}>
         <h1 className="heading">LOGIN</h1>
         <p className="legend"> Please enter your e-mail and password:</p>
-        <input className="inputField" type={"text"} placeholder="Email" />
-        <input className="inputField" type={"text"} placeholder="Password" />
+        <input
+          className="inputField"
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="inputField"
+          type="text"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <p className="forgotPass">
           <NavLink to="/search">Forgot password?</NavLink>
         </p>
-        <button type="button" className="submit">
+        <button type="button" className="submit" onClick={handleSubmit}>
           LOGIN
         </button>
         <p className="hint">
--- frontend/src/modules/Account/Register.js
+++ frontend/src/modules/Account/Register.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Account.scss";
 
 const Register = () => {
+  const [newFirstName, setNewFirstName] = useState("");
+  const [newLastName, setnewLastName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const handleSubmit = () => {
+    console.log("Your Fullname is : " + newFirstName + " " + newLastName);
+    console.log("Your Email is : " + newEmail);
+    console.log("Your Password is : " + newPassword);
+  };
+
   return (
     <div className="register">
       <Navbar />
       <div className="form" style={{ marginTop: "15px" }}>
         <p className="heading">REGISTER</p>
         <p className="legend"> Please fill the information below:</p>
-        <input className="inputField" type={"text"} placeholder="First Name" />
-        <input className="inputField" type={"text"} placeholder="Last Name" />
-        <input className="inputField" type={"text"} placeholder="Email" />
         <input
           className="inputField"
-          type={"password"}
-          placeholder="Password"
+          type="text"
+          placeholder="First Name"
+          onChange={(e) => setNewFirstName(e.target.value)}
         />
-        <button type="button" className="submit">
+        <input
+          className="inputField"
+          type="text"
+          placeholder="Last Name"
+          onChange={(e) => setnewLastName(e.target.value)}
+        />
+        <input
+          className="inputField"
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setNewEmail(e.target.value)}
+        />
+        <input
+          className="inputField"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <button type="button" className="submit" onClick={handleSubmit}>
           CREATE MY ACCOUNT
         </button>
         <p className="hint">
