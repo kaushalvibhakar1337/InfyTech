f76907bed5158247e4c178454204f99088e8c404
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-14 11:46:17 +0530

    Changes

--- frontend/src/components/CategorySlider/CategorySlider.js
+++ frontend/src/components/CategorySlider/CategorySlider.js
@@ -1,17 +1,55 @@
 import React from "react";
+import Slider from "react-slick";
 import "./CategorySlider.scss";
 
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <button className="prevBtn" onClick={onClick}>
+      &lt;
+    </button>
+  );
+};
+
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <button className="nextBtn" onClick={onClick}>
+      &gt;
+    </button>
+  );
+};
+
 const CategorySlider = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
   return (
     <div className="categorySlider">
-      <div className="slider">
-        <div className="categoryCard">
-          <div className="image">
-            <img src="" alt="" className="img1" />
-          </div>
-          <span className="name">NAME</span>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
         </div>
-      </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   );
 };
