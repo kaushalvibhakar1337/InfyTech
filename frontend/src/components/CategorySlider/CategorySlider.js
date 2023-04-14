bea1f4ba7ade3d1a7afe6ef19d50d4ef23d781bf
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-14 16:08:45 +0530

    Changes

--- frontend/src/components/CategorySlider/CategorySlider.js
+++ frontend/src/components/CategorySlider/CategorySlider.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import "./CategorySlider1.scss";
+import "./CategorySlider.scss";
 
 const PrevArrow = (props) => {
   const { onClick } = props;
