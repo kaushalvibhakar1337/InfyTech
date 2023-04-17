446757ad232efecd781dd44f786e6acaa1c9c642
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-17 16:33:28 +0530

    Changes

--- frontend/src/modules/Account/Profile.js
+++ frontend/src/modules/Account/Profile.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { UserAuth } from "../../context/AuthContext";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 import "./Profile.scss";
 
 const Profile = () => {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleLogout = async () => {
     try {
@@ -19,12 +23,32 @@ const Profile = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setFirstName(docSnap.data().FirstName);
+          setLastName(docSnap.data().LastName);
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.log("Error getting document:", error);
+      }
+    };
+    fetchUserData();
+  }, [user]);
+
   return (
     <div className="profile">
       <Navbar />
       <Header title="MY ACCOUNT" />
       <div className="myProfile">
-        <p>Welcome back! {user && user.email}</p>
+        <p>
+          Welcome back, {firstName} {lastName}!
+        </p>
         <button className="logoutBtn" onClick={handleLogout}>
           LOGOUT
         </button>
