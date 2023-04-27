73c81f22480dbe1acf3bf350ce9f33cdfc1a7df4
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-27 12:08:04 +0530

    Changes

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,9 +2,27 @@ import React from "react";
 import "./Header.scss";
 
 const Header = (props) => {
+  let searchBox;
+  if (props.title === "ALL PRODUCTS") {
+    searchBox = (
+      <p className="searchIcon">
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </p>
+    );
+  } else if (props.title === "PRODUCTS") {
+    searchBox = (
+      <p className="searchIcon">
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </p>
+    );
+  } else {
+    searchBox = null;
+  }
+
   return (
     <header>
       <span>{props.title}</span>
+      {searchBox}
     </header>
   );
 };
