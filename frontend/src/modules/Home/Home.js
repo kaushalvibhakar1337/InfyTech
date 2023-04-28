83f35061988ef652bcde1383efe1532eb19e233f
Kaushal Vibhakar <kaushal.vibhakar.1337@gmail.com>
2023-04-28 14:14:50 +0530

    Changes

--- frontend/src/modules/Home/Home.js
+++ frontend/src/modules/Home/Home.js
@@ -3,6 +3,8 @@ import Navbar from "../../components/Navbar/Navbar";
 import Slideshow from "../../components/Slideshow/Slideshow";
 import CategorySlider from "../../components/CategorySlider/CategorySlider";
 import Footer from "../../components/Footer/Footer";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Home.scss";
 
 const Home = () => {
@@ -13,6 +15,29 @@ const Home = () => {
     "slideshowImg4.jpg",
   ];
 
+  const notify = () =>
+    toast.success(
+      <div>
+        THANK YOU FOR SHOPPING WITH US!
+        <br />
+        KINDLY CHECK YOUR EMAIL FOR FURTHER UPDATES ON YOUR ORDER.
+      </div>,
+      {
+        className: "toastify",
+        position: "bottom-right",
+        autoClose: 7000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      }
+    );
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const success = urlParams.get("success") === "true";
+
   return (
     <>
       <Navbar />
@@ -21,6 +46,7 @@ const Home = () => {
         <div className="section">YOUR FAVOURITE PC PARTS IN ONE PLACE</div>
         <CategorySlider />
       </div>
+      {success && notify()}
       <Footer />
     </>
   );
